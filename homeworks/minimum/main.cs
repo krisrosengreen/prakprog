1210132be708033aed5cbd8845e74c0ada55fedd
krisrosengreen <rosengreenpedersen@gmail.com>
2023-05-12 19:57:43 +0200

    Sorta works when given good initial guess

--- homeworks/minimum/main.cs
+++ homeworks/minimum/main.cs
@@ -2,6 +2,19 @@ using System;
 using static System.Math;
 
 
+public class genlist<T>{
+	public T[] data;
+	public int size => data.Length; // property
+	public T this[int i] => data[i]; // indexer
+	public genlist(){ data = new T[0]; }
+	public void add(T item){ /* add item to the list */
+		T[] newdata = new T[size+1];
+		System.Array.Copy(data,newdata,size);
+    newdata[size]=item;
+		data=newdata;
+	}
+}
+
 class funcs {
   public static double simple(vector v) {
     return v.dot(v);
@@ -25,10 +38,27 @@ class funcs {
 
     return Pow(x*x + y - 11, 2) + Pow(x+y*y-7, 2);
   }
+
+  public static double breitwigner(vector x, double E) {
+    double A = x[0];
+    double GAMMA = x[1];
+    double m = x[2];
+
+    return A/(Pow(E-m, 2) + GAMMA*GAMMA / 4);
+  }
+
+  public static double breitwignier_deviation(genlist<double> energy, genlist<double> signal, genlist<double> error, vector x) {
+    double D = 0;
+    for (int i = 0; i < energy.size; i++) {
+      D += Pow((breitwigner(x, energy[i]) - signal[i]) / error[i], 2);
+    }
+
+    return D;
+  }
 }
 
 public class main {
-  public static void Main() {
+  static void partA() {
     vector xmin = minimum.qnewton(funcs.rosenbrock, new vector(-3.0, -3.0), 0.001);
     xmin.print();
     Console.WriteLine($"{funcs.rosenbrock(xmin)}");
@@ -36,5 +66,30 @@ public class main {
     xmin= minimum.qnewton(funcs.himmelblau, new vector(2), 0.001);
     xmin.print();
     Console.WriteLine($"{funcs.himmelblau(xmin)}");
+  }
+
+  static void partB() {
+    var energy = new genlist<double>();
+    var signal = new genlist<double>();
+    var error = new genlist<double>();
+    var seperators = new char[] {' ', '\t'};
+    var options = StringSplitOptions.RemoveEmptyEntries;
+    do {
+      string line = Console.In.ReadLine();
+      if(line == null) break;
+      string[] words = line.Split(seperators, options);
+      energy.add(double.Parse(words[0]));
+      signal.add(double.Parse(words[1]));
+      error.add(double.Parse(words[2]));
+    } while (true);
+
+    Func<vector, double> minimizer = x => funcs.breitwignier_deviation(energy, signal, error, x);
+
+    vector xmin = minimum.qnewton(minimizer, new vector(100.0,100.0,100.0), 0.001);
+    xmin.print();
+  }
+
+  public static void Main() {
+    partB();
   }
 }
